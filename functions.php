36c8a66ad133f23ff25b087b9aef809fa1b637ef
reaganbeck8 <reaganbeck8@gmail.com>
2019-01-22 12:27:23 +0200

    recap on integrating php and html

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+// Variables containing information about myself
+
+$myName = "Reagan Beck";
+
+$age = 21;
+
+// $foods = "curry, chicken stew, pizza";
+
+$foods = "curry, chicken stew, pizza";
+
+$datatype = gettype($myName);
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require('functions.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "Hello my name is $myName and my age is $age . Currently, my favourite foods are $foods. </br> 
+    The variable called $myName is a datatype known as a $datatype" ;
+   ?>  
+</body>
+</html>
